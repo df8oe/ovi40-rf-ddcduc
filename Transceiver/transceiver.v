4ed22dcd9d797897ffd2700460c4df2e607a14ce
DF8OE <df8oe@gmx.de>
2021-02-12 09:45:11 +0100

    initial step to move to final PCB with AD936x, 2nd ADC, PSRAM and USB-PHY

--- Transceiver/transceiver.v
+++ Transceiver/transceiver.v
@@ -2,15 +2,35 @@
 // to enable I2S test pattern generator and pattern detector, uncomment next line
 // `define DEBUG_I2S
 module Transceiver(
-    // ADC interface
+    // ADC1 interface
     input [13:0] adc_data,
-    input adc_clock,
-    input adc_overrange,
+    input adc1_clock,
+    input adc1_overrange,
+
+    // ADC2 interface
+    input [13:0] adc2_data,
+    input adc2_clock,
+    input adc2_overrange,
 
     // DAC interface
     output [13:0] dac_data,
     output dac_clock,
     output reg level_pwm,
+
+    // AD936x interface
+    inout [11:0] p0_data,
+    inout [11:0] p1_data,
+    output tx_frame_p,
+    input rx_frame_p,
+    output fb_clk_p,
+    output txnrx,
+    input data_clk_p,
+    output enable,
+    output nresb,
+    output nspi_enb,
+    output spi_clk,
+    output spi_di,
+    input spi_do,
 
     // I2S bus, master mode
     input DIN,
@@ -31,19 +51,33 @@ module Transceiver(
     input CW,
     output nRES,
     output OF,
-    input _10M_in,
-    output _10M_out,
     input clock_10M,
-    output dummy_1,
-    output dummy_2,
+    input ext_10M_in,
+
+    // USB PHY
+    inout [7:0] usb_data,
+    input usb_dir,
+    input usb_nxt,
+    output usb_stp,
+    output usb_resetb,
+    output usb_refclk,
+
+    // PSRAM
+    inout [7:0] ram_data,
+    output ram_rwds,
+    output ram_ncs,
+    output ram_nck,
+    output ram_ck,
 
     // Test pins
     output led1,
     output led2,
+    output led3,
     output test1,
     output test2,
     output test3,
-    output test4
+    output test4,
+    output test5
 
     );
 
@@ -54,20 +88,16 @@ module Transceiver(
 
 
     assign dac_clock = main_clock;
-    assign _10M_out = _10M_in;
     assign MCLK = ~_mclk & lock_rx;
     assign nRES = reset;
-	 
-	 wire _mclk;
-	 
-	 div2_posedge(.reset(reset), .clk(SAICLK), .div_clk(_mclk));
-	 
-//    assign dummy_1 = 0;
-//    assign dummy_2 = 0;
+
+    wire _mclk;
+
+    div2_posedge(.reset(reset), .clk(SAICLK), .div_clk(_mclk));
 
     // PLL 1
     wire main_clock, SAICLK, lock_rx;
-    pll_rx prx (adc_clock, SAICLK, main_clock, lock_rx);
+    pll_rx prx (adc1_clock, SAICLK, main_clock, lock_rx);
 
     // PLL 2
     wire clock_100k, clock_2M, lock_10;
@@ -90,19 +120,19 @@ module Transceiver(
 
     //
     wire clipping;
-    clip_led cl (clock_100k, adc_overrange | dac_of, clipping);
-	 
+    clip_led cl (clock_100k, adc1_overrange | dac_of, clipping);
+
     wire i2s_ok;
-`ifdef DEBUG_I2S	 
+`ifdef DEBUG_I2S
     assign led1 = i2s_ok;
-`else	 
+`else
     assign led1 = clipping ;
-`endif	 
+`endif
     assign OF = clipping;
 
     // read and register ADC data
     reg [13:0] reg_adc_data;
-    always @(posedge adc_clock)
+    always @(posedge adc1_clock)
     begin
         reg_adc_data <=  adc_data[13:0];
     end

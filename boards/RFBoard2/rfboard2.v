1ff59930e9759ec6f30ff71203365b44fd8b81b5
DF8OE <df8oe@gmx.de>
2021-08-17 09:00:55 +0200

    finished pin assignments of RF2

--- boards/RFBoard2/rfboard2.v
+++ boards/RFBoard2/rfboard2.v
@@ -2,10 +2,45 @@
 // to enable I2S test pattern generator and pattern detector, uncomment next line
 // `define DEBUG_I2S
 module RFBoard2(
-    // ADC interface
+    // ADC interfaces
     input [13:0] adc_data,
     input adc_clock,
     input adc_overrange,
+
+    input [13:0] adc2_data,
+    input adc2_clock,
+    input adc2_overrange,
+
+	// AD936x interface
+	output [11:0] ad936x_p0,
+	input [11:0] ad936x_p1,
+	input rx_frame_p,
+	output enable,
+	output nresetb,
+	output tx_frame_p,
+	output fb_clk_p,
+	output txnrx,
+	input data_clk_p,
+	output nspi_enb,
+	output spi_clk,
+	output spi_di,
+	input spi_do,
+
+	// RAM interface
+	inout [7:0] ram,
+	output ram_ncs,
+	output ram_nreset,
+	output ram_nck,
+	output ram_ck,
+	inout ram_rwds,
+
+	// USB interface
+	inout [7:0] usb,
+	input usb_dir,
+	input usb_nxt,
+	output usb_refclk,
+	output usb_resetb,
+	output usb_stp,
 
     // DAC interface
     output [13:0] dac_data,
@@ -18,6 +53,7 @@ module RFBoard2(
     output BCLK,
     output LRCLK,
     output MCLK,
+    inout RESET,
 
     // I2C slave control bus
     inout slave_SDA,
@@ -32,25 +68,25 @@ module RFBoard2(
     output nRES,
     output OF,
     input _10M_in,
+    output smps_13_enable,
     output _10M_out,
     input clock_10M,
-    output dummy_1,
-    output dummy_2,
 
     // Test pins
     output led1,
     output led2,
+    output led3,
     output test1,
-    output test2,
+    input test2,
     output test3,
-    output test4
-
+    output test4,
+    output test5
     );
 	 
     transceiver
 	 
 	 trx(
-    // ADC interface
+    // ADC interfaces
     .adc_data(adc_data),
     .adc_clock(adc_clock),
     .adc_overrange(adc_overrange),
@@ -82,8 +118,6 @@ module RFBoard2(
     ._10M_in(_10M_in),
     ._10M_out(_10M_out),
     .clock_10M(clock_10M),
-    .dummy_1(dummy_1),
-    .dummy_2(dummy_2),
 
     // Test pins
     .led1(led1),
